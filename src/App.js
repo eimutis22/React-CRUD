4b77164d13dc228578ebc99210f62218c5b5f1d6
Eim <eimutis.genys@gmail.com>
2018-11-27 19:41:52 +0000

    CRUD - GET

--- src/App.js
+++ src/App.js
@@ -1,11 +1,68 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const reports = [
+  {
+    id: 1,
+    timestamp: '2018-11-20',
+    product: 'broadband',
+    issue: 'slow',
+    town: 'sligo',
+    caller_phone_number: null
+  },
+  {
+    id: 2,
+    timestamp: '2018-11-20',
+    product: 'broadband',
+    issue: 'not_working',
+    town: 'dublin',
+    caller_phone_number: null
+  },
+  {
+    id: 3,
+    timestamp: '2018-11-22',
+    product: 'landline',
+    issue: 'not_working',
+    town: 'sligo',
+    caller_phone_number: null
+  },
+];
+
+localStorage.setItem('reports', JSON.stringify(reports));
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reports: []
+    };
+  }
+  
+  componentWillMount() {
+    this.getReports();    
+  }
+
+  getReports() {
+    const localReports = JSON.parse(localStorage.getItem('reports'));
+    this.state.reports = localReports;
+    console.log('Reports: ', this.state.reports);
+  }
+
   render() {
     return (
       <div className="App">
-        Hello
+        <h1>Reports</h1>
+        {
+          this.state.reports.map(report => {
+            return(
+              <div key={report.id}>
+                <span>{report.timestamp}</span> | <span>{report.product}</span> | <span>{report.issue}</span> | <span>{report.town}</span>
+              </div>
+            );
+          })
+        }
       </div>
     );
   }
