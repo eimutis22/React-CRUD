8a794b5b0dc373ffe1578bd289509f7b9a1bf6e2
Eim <eimutis.genys@gmail.com>
2018-11-27 17:57:49 +0000

    *** RESET TO BEGINNING

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React, { Component } from 'react';
-import MyComponent from './MyComponent';
 import './App.css';
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      renderTitle: true,
-      title: 'Awesome Title'
-    }
-  }
-
-  renderTitle() {
-    if (!this.state.renderTitle)
-      return null;
-
-    return <h1>{this.state.title}</h1>;
-  }
-
   render() {
     return (
       <div className="App">
-
-        {this.renderTitle()}
-
+        Hello
       </div>
     );
   }
